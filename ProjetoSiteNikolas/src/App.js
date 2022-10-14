dbb2cb05172f63404869b0b32ec9d880e25e9fff
NikolasSilvaLunkes <111617750+NikolasSilvaLunkes@users.noreply.github.com>
2022-10-13 21:42:00 -0300

    Add files via upload

---
+++ ProjetoSiteNikolas/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './content/Home'
+import Background from './content/Background';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Background/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
